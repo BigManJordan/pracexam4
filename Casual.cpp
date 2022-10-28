2da7d042dcbfe049f5465a36e0c79a0bf6bc21e7
BigManJordan <100400782+BigManJordan@users.noreply.github.com>
2022-10-28 15:41:30 +1030

    Add files via upload

---
+++ Casual.cpp
@@ -0,0 +1,8 @@
+//
+//  Casual.cpp
+//  pracexam4
+//
+//  Created by Jordan Le on 28/10/2022.
+//
+
+#include "Casual.h"
---
+++ Casual.hpp
@@ -0,0 +1,13 @@
+//
+//  Casual.hpp
+//  pracexam4
+//
+//  Created by Jordan Le on 28/10/2022.
+//
+
+#ifndef Casual_h
+#define Casual_h
+
+#include <stdio.h>
+
+#endif
